9f2b96c8b08e3b5addc02ec418f7c259f3a84e7c
etienne <etiennerodriguez3@gmail.com>
2022-09-13 12:31:45 -0400

    added render functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,9 @@
 import "./form";
-import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { initDb, getDb, postDb } from './database';
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -10,9 +11,7 @@ import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
   initDb()
-  getDb();
-  postDb("Etienne", "etienne@mail.com", 4440009999, "Monkey");
-  getDb();
+  fetchCards();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
@@ -20,3 +19,39 @@ window.addEventListener('load', function () {
 
     // Import CSS files
     import "../css/index.css";
+
+      // Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+ })
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let email = document.getElementById("email").value;
+let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+if (submitBtnToUpdate == false) {
+  postDb(name, email, phone, profile);
+} else {
+
+  fetchCards();
+    // Toggles the submit button back to POST functionality
+  submitBtnToUpdate = false;
+}
+
+// Clear form
+clearForm();
+// Toggle form
+toggleForm();
+// Reload the DOM
+fetchCards();
+});
