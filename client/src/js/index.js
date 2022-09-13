f5898204cd43ce6fb8dfd9027032b8b26aed575d
etienne <etiennerodriguez3@gmail.com>
2022-09-13 11:50:42 -0400

    added indexed api

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,12 +2,14 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+  initdb(); 
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
